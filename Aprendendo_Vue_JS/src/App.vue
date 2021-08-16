b34ef1760f3d6abb75e585db20e852dce1413158
Gu3001 <gustavo180g@gmail.com>
2021-08-16 09:15:27 -0300

    Projeto contador de curtidas em VUE JS

---
+++ Aprendendo_Vue_JS/src/App.vue
@@ -0,0 +1,61 @@
+
+<template>
+  
+
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Teste aplicação VUE, contador de likes"/>
+  <h2 v-if="likes > 0 && likes < 10">
+    Esse post teve {{likes}} 👍😁
+  </h2>
+  <h2 v-else-if="likes >= 10">
+    Esse post teve {{likes}} 👍 Parabéns!!! 🥳
+  </h2>
+  <h2 v-else >
+    Nenhum Like até agora! 😥
+  </h2>
+
+  <button v-on:click="aumentarLikes()" class="botaoLike" >Curtir</button>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+   data() {
+      return {
+        likes: 0
+    }    
+},
+methods: {
+      aumentarLikes(){
+        this.likes++;
+      }
+    }
+}
+</script>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.botaoLike {
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
